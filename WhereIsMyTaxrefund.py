fc42177a25697ebe47ef441b64373ac35eac5be9
sudhichai <oh@isecthailand.com>
2018-07-09 00:26:39 +0700

    Add iteration and CSV module

--- WhereIsMyTaxrefund.py
+++ WhereIsMyTaxrefund.py
@@ -1,10 +1,12 @@
 
 from selenium import webdriver
+import  csv
 browser =  webdriver.Chrome()
 
 browser.get('https://sa.www4.irs.gov/irfof/lang/en/irfofgetstatus.jsp')
 
 browser.switch_to.alert.accept()
+
 
 TIN3 = browser.find_element_by_id('TIN3')
 TIN2 = browser.find_element_by_id('TIN2')
@@ -13,6 +15,15 @@ TIN5 = browser.find_element_by_id('TIN5')
 Single = browser.find_element_by_id('filingStatus1')
 Refund = browser.find_element_by_id('refundAmount')
 Submit = browser.find_element_by_id('Submit2')
+
+socialFile = open('social.csv')
+rowReader = csv.reader(socialFile)
+for row in rowReader:
+    TIN3.send_keys(rowReader[row][0].split('-')[0])
+    TIN2.send_keys(rowReader[row][0].split('-')[1])
+    TIN5.send_keys(rowReader[row][0].split('-')[2])
+
+
 
 TIN3.send_keys('687')
 TIN2.send_keys('94')

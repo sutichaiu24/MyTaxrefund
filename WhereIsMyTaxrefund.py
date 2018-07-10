67c0dc3c239274a3346fdbb0871a1f87a7970536
sutichaiu24 <oh@isecthailand.com>
2018-07-10 13:08:10 +0700

    Line notify add

--- WhereIsMyTaxrefund.py
+++ WhereIsMyTaxrefund.py
@@ -2,6 +2,12 @@
 from selenium import webdriver
 import  time
 import  csv
+import requests,json
+import urllib.parse
+
+LINE_ACCESS_TOKEN="LQNe4sqZ5ouBgWjJpSOFO9FLpGxUB4WIvyTE0t0hAiR"
+url = "https://notify-api.line.me/api/notify"
+
 browser =  webdriver.Chrome()
 
 browser.get('https://sa.www4.irs.gov/irfof/lang/en/irfofgetstatus.jsp')
@@ -33,7 +39,7 @@ for row in rowReader :
       Submit.click()
       if browser.find_element_by_id("contentpage_lesswhitespace"):
             result = browser.find_element_by_id("contentpage_lesswhitespace").text
-            print(result)
+            def linenotify (result)
       else:
                 print ("Sorry ")
 
@@ -43,8 +49,15 @@ browser.close()
 
 
 
+def linenotify (botmessage) :
+      message ="botmessage  # ข้อความที่ต้องการส่ง
+      msg = urllib.parse.urlencode({"message":message})
+      LINE_HEADERS = {'Content-Type':'application/x-www-form-urlencoded',"Authorization":"Bearer "+LINE_ACCESS_TOKEN}
+      session = requests.Session()
+      a=session.post(url, headers=LINE_HEADERS, data=msg)
+      print(a.text)
 
 
-browser.close()
+
 
 
